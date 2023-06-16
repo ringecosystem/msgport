056230008959cd4a0b905ef2af9bcb2b7c17d964
Aki Wu <wuminzhe@gmail.com>
2023-06-16 15:13:20 +0800

    remove unnecessary code from layerzero doc

--- contracts/docks/LayerZeroDock.sol
+++ contracts/docks/LayerZeroDock.sol
@@ -124,30 +124,4 @@ contract LayerZeroDock is
 
         recv(fromDappAddress, inboundLane, toDappAddress, messagePayload);
     }
-
-    event Debug(bytes);
-
-    function lzReceive(
-        uint16 _srcChainId,
-        bytes calldata _srcAddress,
-        uint64 _nonce,
-        bytes calldata _payload
-    ) public override {
-        // lzReceive must be called by the endpoint for security
-        require(
-            _msgSender() == address(lzEndpoint),
-            "LayerZeroDock: invalid endpoint caller"
-        );
-
-        bytes memory trustedRemote = trustedRemoteLookup[_srcChainId];
-        // if will still block the message pathway from (srcChainId, srcAddress). should not receive message from untrusted remote.
-        require(
-            _srcAddress.length == trustedRemote.length &&
-                trustedRemote.length > 0 &&
-                keccak256(_srcAddress) == keccak256(trustedRemote),
-            "LzApp: invalid source sending contract"
-        );
-
-        _blockingLzReceive(_srcChainId, _srcAddress, _nonce, _payload);
-    }
 }
