a86a57fd73e58aad97ae397df2ab48c216f870b9
echo <hujw77@gmail.com>
2023-10-08 15:28:43 +0800

    comment

--- contracts/examples/ExampleReceiverDapp.sol
+++ contracts/examples/ExampleReceiverDapp.sol
@@ -9,10 +9,14 @@ contract ExampleReceiverDapp is Application {
         bytes32 messageId, uint256 fromChainId, address fromDappAddress, address localLineAddress, bytes message
     );
 
+    // local line address
     address public immutable LINE;
+    // remote dapp address
+    address public immutable DAPP;
 
-    constructor(address line) {
+    constructor(address line, address dapp) {
         LINE = line;
+        DAPP = dapp;
     }
 
     function xxx(bytes calldata message) external {
@@ -21,6 +25,7 @@ contract ExampleReceiverDapp is Application {
         address fromDappAddress = _xmsgSender();
         address localLineAddress = _msgSender();
         require(localLineAddress == LINE);
+        require(fromDappAddress == DAPP);
         emit DappMessageRecv(messageId, fromChainId, fromDappAddress, localLineAddress, message);
     }
 }
