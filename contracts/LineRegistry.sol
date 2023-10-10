d9bdb6d5d846b2a917fbfecd0189a6300e554ed7
Guantong <i@gt.email>
2023-10-10 17:25:27 +0800

    Fix review

--- contracts/LineRegistry.sol
+++ contracts/LineRegistry.sol
@@ -3,23 +3,19 @@
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/access/Ownable2Step.sol";
-import "./lines/base/LineMetadata.sol";
+import "./interfaces/ILineMetadata.sol";
 
 contract LineRegistry is Ownable2Step {
-    // lineName => localLineAddress
-    mapping(string => address) private _localLineLookup;
+    // lineName => lineAddress
+    mapping(string => address) private _lineLookup;
 
-    function LOCAL_CHAINID() public view returns (uint256) {
-        return block.chainid;
+    function getLine(string memory name) external view returns (address) {
+        return _lineLookup[name];
     }
 
-    function getLocalLine(string memory name) external view returns (address) {
-        return _localLineLookup[name];
-    }
-
-    function addLocalLine(address localLine) external onlyOwner {
-        string memory name = LineMetadata(localLine).name();
-        require(_localLineLookup[name] == address(0), "Line name already exists");
-        _localLineLookup[name] = localLine;
+    function addLine(address line) external onlyOwner {
+        string memory name = ILineMetadata(line).name();
+        require(_lineLookup[name] == address(0), "Line name already exists");
+        _lineLookup[name] = line;
     }
 }
