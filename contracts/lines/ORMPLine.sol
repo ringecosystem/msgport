3da67de088cc87a775b86266ef84d9902d26055a
echo <hujw77@gmail.com>
2023-10-11 11:33:21 +0800

    Fix ormp line

--- contracts/lines/ORMPLine.sol
+++ contracts/lines/ORMPLine.sol
@@ -2,32 +2,22 @@
 pragma solidity ^0.8.17;
 
 import "./base/BaseMessageLine.sol";
+import "ORMP/interfaces/IEndpoint.sol";
+import "ORMP/user/Application.sol";
 
-interface IORMP {
-    function send(uint256 toChainId, address to, bytes calldata encoded, bytes calldata params)
-        external
-        payable
-        returns (bytes32);
-}
-
-contract ORMPLine is BaseMessageLine {
-    address public immutable ORMP;
-
-    constructor(address ormp, Metadata memory metadata) BaseMessageLine(metadata) {
-        ORMP = ormp;
-    }
+contract ORMPLine is BaseMessageLine, Application {
+    constructor(address ormp, Metadata memory metadata) BaseMessageLine(metadata) Application(ormp) {}
 
     function _send(address fromDapp, uint256 toChainId, address toDapp, bytes memory message, bytes memory params)
         internal
         override
     {
-        bytes memory encoded =
-            abi.encodeWithSelector(ORMPLine.recv.selector, LOCAL_CHAINID(), fromDapp, toDapp, message);
-        IORMP(ORMP).send{value: msg.value}(toChainId, ORMP, encoded, params);
+        bytes memory encoded = abi.encodeWithSelector(ORMPLine.recv.selector, fromDapp, toDapp, message);
+        IEndpoint(TRUSTED_ORMP).send{value: msg.value}(toChainId, address(this), encoded, params);
     }
 
-    function recv(uint256 fromChainId, address fromDapp, address toDapp, bytes memory message) external {
-        require(msg.sender == ORMP, "!auth");
-        _recv(fromChainId, fromDapp, toDapp, message);
+    function recv(address fromDapp, address toDapp, bytes memory message) external {
+        require(_xmsgSender() == address(this), "!auth");
+        _recv(_fromChainId(), fromDapp, toDapp, message);
     }
 }
