b8a1547578f3e4649c0c06917b330883ac7f30e0
echo <hujw77@gmail.com>
2023-10-19 09:55:20 +0800

    typo

--- bin/add.sh
+++ bin/add.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-export LINE_FILE="deploy_ormp_line.a"
+export LINE_DEPLOY_FILE="deploy_ormp_line.a"
 export LINE_KEY=".ORMP_LINE"
 forge script script/config/RegistryLine.s.sol:RegistryLine --chain-id 44     --broadcast
 forge script script/config/RegistryLine.s.sol:RegistryLine --chain-id 421614 --broadcast --skip-simulation
