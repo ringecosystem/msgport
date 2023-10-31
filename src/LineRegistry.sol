ba21993c269bb404640e6b1be820ad04ac96faf7
Guantong <i@gt.email>
2023-10-31 15:37:06 +0800

    Docs (#156)
    * docs
    
    * Fix review

--- src/LineRegistry.sol
+++ src/LineRegistry.sol
@@ -1,10 +1,28 @@
-// SPDX-License-Identifier: MIT
+// This file is part of Darwinia.
+// Copyright (C) 2018-2023 Darwinia Network
+// SPDX-License-Identifier: GPL-3.0
+//
+// Darwinia is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Darwinia is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Darwinia. If not, see <https://www.gnu.org/licenses/>.
 
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/access/Ownable2Step.sol";
 import "./interfaces/ILineMetadata.sol";
 
+/// @title LineRegistry
+/// @notice LineRegistry will be deployed on each chain. 
+///         It is the registry of messageLine and can be used to verify whether the line has been registered.
 contract LineRegistry is Ownable2Step {
     // lineName => lineAddress
     mapping(string => address) private _lineLookup;
--- src/lines/base/BaseMessageLine.sol
+++ src/lines/base/BaseMessageLine.sol
@@ -1,4 +1,19 @@
-// SPDX-License-Identifier: MIT
+// This file is part of Darwinia.
+// Copyright (C) 2018-2023 Darwinia Network
+// SPDX-License-Identifier: GPL-3.0
+//
+// Darwinia is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Darwinia is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Darwinia. If not, see <https://www.gnu.org/licenses/>.
 
 pragma solidity ^0.8.0;
 
@@ -12,6 +27,13 @@ abstract contract BaseMessageLine is IMessageLine, LineMetadata {
         return block.chainid;
     }
 
+    /// @dev Send a cross-chain message over the MessageLine.
+    ///      Line developer should implement this, then it will be called by `send`.
+    /// @param fromDapp The real sender account who send the message.
+    /// @param toChainId The message destination chain id. <https://eips.ethereum.org/EIPS/eip-155>
+    /// @param toDapp The user application contract address which receive the message.
+    /// @param message The calldata which encoded by ABI Encoding.
+    /// @param params Extend parameters to adapt to different message protocols.
     function _send(address fromDapp, uint256 toChainId, address toDapp, bytes calldata message, bytes calldata params)
         internal
         virtual;
@@ -20,6 +42,12 @@ abstract contract BaseMessageLine is IMessageLine, LineMetadata {
         _send(msg.sender, toChainId, toDapp, message, params);
     }
 
+    /// @dev Make toDapp accept messages.
+    ///      This should be called by message line when a message is received.
+    /// @param fromChainId The source chainId, standard evm chainId.
+    /// @param fromDapp The message sender in source chain.
+    /// @param toDapp The message receiver in dest chain.
+    /// @param message The message body.
     function _recv(uint256 fromChainId, address fromDapp, address toDapp, bytes memory message)
         internal
         returns (bytes memory)
