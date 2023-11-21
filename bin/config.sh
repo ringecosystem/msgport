4f447cd025e0dcfaf5c18814736bbf8862f5af8d
echo <hujw77@gmail.com>
2023-11-21 15:02:26 +0800

    ormpline config

--- bin/config.sh
+++ bin/config.sh
@@ -12,5 +12,9 @@ uri="ipfs:://$cid"
 # forge script script/config/LineConfig.s.sol:LineConfig --sig "run(uint256[],string)" "[42161]" $uri --chain-id 46    --broadcast --slow
 # forge script script/config/LineConfig.s.sol:LineConfig --sig "run(uint256[],string)" "[46]"    $uri --chain-id 42161 --broadcast --slow --legacy
 
-forge script script/config/LineConfig.s.sol:LineConfig --sig "run(uint256[],string)" "[44]"       $uri --chain-id 11155111 --broadcast
+forge script script/config/LineConfig.s.sol:LineConfig --sig "run(uint256[],string)" "[421614]"   $uri --chain-id 44       --broadcast
 forge script script/config/LineConfig.s.sol:LineConfig --sig "run(uint256[],string)" "[11155111]" $uri --chain-id 44       --broadcast
+forge script script/config/LineConfig.s.sol:LineConfig --sig "run(uint256[],string)" "[11155111]" $uri --chain-id 421614   --broadcast --skip-simulation --legacy
+forge script script/config/LineConfig.s.sol:LineConfig --sig "run(uint256[],string)" "[44]"       $uri --chain-id 421614   --broadcast --skip-simulation --legacy
+forge script script/config/LineConfig.s.sol:LineConfig --sig "run(uint256[],string)" "[44]"       $uri --chain-id 11155111 --broadcast
+forge script script/config/LineConfig.s.sol:LineConfig --sig "run(uint256[],string)" "[421614]"   $uri --chain-id 11155111 --broadcast
