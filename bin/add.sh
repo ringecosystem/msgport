74267f60cc0b52b53d38d88e0d8acf87cece360f
echo <hujw77@gmail.com>
2023-11-21 15:05:07 +0800

    Deploy on sepolia (#176)
    * deploy on sepolia
    
    * commnet
    
    * ormpline config

--- bin/add.sh
+++ bin/add.sh
@@ -4,5 +4,6 @@ set -eo pipefail
 
 export LINE_DEPLOY_FILE="deploy_ormp_line.a"
 export LINE_KEY=".ORMP_LINE"
-forge script script/config/RegistryLine.s.sol:RegistryLine --chain-id 46    --broadcast --slow
-forge script script/config/RegistryLine.s.sol:RegistryLine --chain-id 42161 --broadcast --slow --legacy
+# forge script script/config/RegistryLine.s.sol:RegistryLine --chain-id 46    --broadcast --slow
+# forge script script/config/RegistryLine.s.sol:RegistryLine --chain-id 42161 --broadcast --slow --legacy
+forge script script/config/RegistryLine.s.sol:RegistryLine --chain-id 11155111 --broadcast
