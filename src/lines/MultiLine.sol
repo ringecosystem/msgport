ce2b705f66bc56d0296c4ec5ae3677ef0b935803
echo <hujw77@gmail.com>
2024-01-15 18:13:01 +0800

    multi line changes

--- src/lines/MultiLine.sol
+++ src/lines/MultiLine.sol
@@ -32,16 +32,25 @@ contract MultiLine is Ownable2Step, Application, BaseMessageLine, LineLookup {
     using EnumerableSet for EnumerableSet.AddressSet;
 
     /// @dev RemoteCallArgs
-    /// @param params Params correspond with the trusted lines.
-    /// @param fees Fees correspond with the trusted lines.
+    /// @param lines Message lines selected for send the message.
+    /// @param nonce Nonce for lineMsgId uniqueness.
+    /// @param expiration The message considered be stale after the expiration in the number seconds.
+    /// @param params Params correspond with the selected lines.
+    /// @param fees Fees correspond with the selected lines.
     struct RemoteCallArgs {
+        address[] lines;
+        uint256 nonce;
+        uint256 expiration;
         bytes[] params;
         uint256[] fees;
     }
 
     struct MultiSendArgs {
+        address[] lines;
         uint256 toChainId;
         address toDapp;
+        uint256 nonce;
+        uint256 expiration;
         bytes message;
         bytes[] params;
         uint256[] fees;
@@ -57,20 +66,26 @@ contract MultiLine is Ownable2Step, Application, BaseMessageLine, LineLookup {
         bytes message;
     }
 
+    /// @dev Nonce for lineMsgId uniqueness.
     uint256 public nonce;
+    /// @dev Threshold for multi line confirmation to execute msg.
     uint256 public threshold;
+    /// @dev The message considered be stale after the expiration time in the number seconds.
     uint256 public expiration;
+    /// @dev Trusted lines managed by dao.
     EnumerableSet.AddressSet private _trustedLines;
 
     mapping(bytes32 lineMsgId => bool done) public doneOf;
     mapping(bytes32 lineMsgId => uint256 deliveryCount) public countOf;
-    // protect msg repeat by underwood
+    // protect msg repeat by msgline
     mapping(bytes32 lineMsgId => mapping(address line => bool isDeliveried)) public deliverifyOf;
 
+    /// @dev The maximum duration that a message's expiration parameter can be set to
+    uint256 public constant MAX_MESSAGE_EXPIRATION = 30 days;
+
     event SetThreshold(uint256 threshold);
-    event SetExpiration(uint256 expiration);
-    event LineMessageSent(bytes32 indexed lineMsgId, LineMsg lineMsg);
-    event LineMessageConfirmation(bytes32 indexed lineMsgId, string name);
+    event LineMessageSent(bytes32 indexed lineMsgId, LineMsg lineMsg, bool[] sentResult);
+    event LineMessageConfirmation(bytes32 indexed lineMsgId, address line);
     event LineMessageExpired(bytes32 indexed lineMsgId);
     event LineMessageExecution(bytes32 indexed lineMsgId);
 
@@ -91,11 +106,6 @@ contract MultiLine is Ownable2Step, Application, BaseMessageLine, LineLookup {
         require(threshold_ > 0, "!threshold");
         threshold = threshold_;
         emit SetThreshold(threshold_);
-    }
-
-    function setExpiration(uint256 expiration_) external onlyOwner {
-        expiration = expiration_;
-        emit SetExpiration(expiration_);
     }
 
     function addTrustedLine(address line) external onlyOwner {
@@ -140,17 +150,17 @@ contract MultiLine is Ownable2Step, Application, BaseMessageLine, LineLookup {
         override
     {
         RemoteCallArgs memory args = abi.decode(params, (RemoteCallArgs));
-        multiSend(MultiSendArgs(toChainId, toDapp, message, args.params, args.fees));
+        multiSend(
+            MultiSendArgs(args.lines, toChainId, toDapp, args.nonce, args.expiration, message, args.params, args.fees)
+        );
     }
 
     function multiSend(MultiSendArgs memory args) public payable {
-        uint256 len = trustedLineCount();
+        uint256 len = args.lines.length;
         require(args.toChainId != LOCAL_CHAINID(), "!toChainId");
-        require(len >= threshold, "!len");
         require(len == args.params.length, "!len");
         require(len == args.fees.length, "!len");
-
-        ++nonce;
+        require(block.timestamp + MAX_MESSAGE_EXPIRATION > args.expiration, "!expiration");
 
         address fromDapp = msg.sender;
         LineMsg memory lineMsg = LineMsg({
@@ -158,30 +168,42 @@ contract MultiLine is Ownable2Step, Application, BaseMessageLine, LineLookup {
             toChainId: args.toChainId,
             fromDapp: fromDapp,
             toDapp: args.toDapp,
-            nonce: nonce,
-            expiration: block.timestamp + expiration,
+            nonce: args.nonce,
+            expiration: args.expiration,
             message: args.message
         });
         bytes memory encoded = abi.encodeWithSelector(MultiLine.multiRecv.selector, lineMsg);
         bytes32 lineMsgId = hash(lineMsg);
 
-        _multiSend(args, encoded);
-        emit LineMessageSent(lineMsgId, lineMsg);
+        bool[] memory sentResult = _multiSend(args, encoded);
+        emit LineMessageSent(lineMsgId, lineMsg, sentResult);
     }
 
-    function _multiSend(MultiSendArgs memory args, bytes memory encoded) internal {
-        address[] memory lines = trustedLines();
-        uint256 len = lines.length;
+    function _multiSend(MultiSendArgs memory args, bytes memory encoded) internal returns (bool[] memory) {
+        uint256 len = args.lines.length;
         uint256 totalFee = 0;
+        bool[] memory sentResult = new bool[](len);
         for (uint256 i = 0; i < len; i++) {
             uint256 fee = args.fees[i];
-            address line = lines[i];
+            address line = args.lines[i];
             require(isTrustedLine(line), "!trusted");
-            IMessageLine(line).send{value: fee}(args.toChainId, _toLine(args.toChainId), encoded, args.params[i]);
+            sentResult[i] = _sendMessage(line, fee, args.toChainId, encoded, args.params[i]);
             totalFee += fee;
         }
 
         require(totalFee == msg.value, "!fees");
+        return sentResult;
+    }
+
+    function _sendMessage(address line, uint256 fee, uint256 toChainId, bytes memory encoded, bytes memory params)
+        internal
+        returns (bool r)
+    {
+        try IMessageLine(line).send{value: fee}(toChainId, _toLine(toChainId), encoded, params) {
+            r = true;
+        } catch {
+            r = false;
+        }
     }
 
     function multiRecv(LineMsg calldata lineMsg) external payable {
@@ -197,7 +219,7 @@ contract MultiLine is Ownable2Step, Application, BaseMessageLine, LineLookup {
         deliverifyOf[lineMsgId][line] = true;
         ++countOf[lineMsgId];
 
-        emit LineMessageConfirmation(lineMsgId, ILineMetadata(line).name());
+        emit LineMessageConfirmation(lineMsgId, line);
 
         if (block.timestamp > lineMsg.expiration) {
             emit LineMessageExpired(lineMsgId);
