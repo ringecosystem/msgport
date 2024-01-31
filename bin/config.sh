e7579cc1c2f64dcf3d6f99f947c02c980f947a69
echo <hujw77@gmail.com>
2024-01-31 14:33:46 +0800

    code

--- bin/config.sh
+++ bin/config.sh
@@ -13,12 +13,9 @@ uri="ipfs://$cid"
 # forge script script/config/PortConfig.s.sol:PortConfig --sig "run(uint256[],string)" "[46]"    $uri --chain-id 42161 --broadcast --slow --legacy
 # forge script script/config/PortConfig.s.sol:PortConfig --sig "run(uint256[],string)" "[46]"    $uri --chain-id 1 --broadcast --slow --legacy
 
-forge script script/config/PortConfig.s.sol:PortConfig --sig "run(uint256[],string)" "[421614]"   $uri --chain-id 44       --broadcast
-forge script script/config/PortConfig.s.sol:PortConfig --sig "run(uint256[],string)" "[11155111]" $uri --chain-id 44       --broadcast
-forge script script/config/PortConfig.s.sol:PortConfig --sig "run(uint256[],string)" "[11155111]" $uri --chain-id 421614   --broadcast --skip-simulation --legacy
-forge script script/config/PortConfig.s.sol:PortConfig --sig "run(uint256[],string)" "[44]"       $uri --chain-id 421614   --broadcast --skip-simulation --legacy
-forge script script/config/PortConfig.s.sol:PortConfig --sig "run(uint256[],string)" "[44]"       $uri --chain-id 11155111 --broadcast
-forge script script/config/PortConfig.s.sol:PortConfig --sig "run(uint256[],string)" "[421614]"   $uri --chain-id 11155111 --broadcast
+forge script script/config/PortConfig.s.sol:PortConfig --sig "run(uint256[],string)" "[421614,11155111]" $uri --chain-id 44       --broadcast
+forge script script/config/PortConfig.s.sol:PortConfig --sig "run(uint256[],string)" "[11155111,44]"     $uri --chain-id 421614   --broadcast --skip-simulation --legacy
+forge script script/config/PortConfig.s.sol:PortConfig --sig "run(uint256[],string)" "[44,421614]"       $uri --chain-id 11155111 --broadcast
 
 # forge script script/config/PortConfig.s.sol:PortConfig --sig "run(uint256[],string)" "[11155111]" $uri --chain-id 44       --broadcast
 # forge script script/config/PortConfig.s.sol:PortConfig --sig "run(uint256[],string)" "[44]"       $uri --chain-id 11155111 --broadcast
