0d67ab0488a536276bd6c08ce07c588a28593af3
echo <hujw77@gmail.com>
2024-01-31 11:49:02 +0800

    fix

--- bin/add.sh
+++ bin/add.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-export PORT_DEPLOY_FILE="deploy_ormp_line.a"
+export PORT_DEPLOY_FILE="deploy_ormp_port.a"
 export PORT_KEY=".ORMP_PORT"
 # forge script script/config/RegistryPort.s.sol:RegistryPort --chain-id 46    --broadcast --slow
 # forge script script/config/RegistryPort.s.sol:RegistryPort --chain-id 42161 --broadcast --slow --legacy
