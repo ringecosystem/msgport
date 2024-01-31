d3830260d0f5834ea5b8b7fe436bdcb3c6f9368f
echo <hujw77@gmail.com>
2024-01-31 11:29:02 +0800

    rename

--- bin/config.sh
+++ bin/config.sh
@@ -2,24 +2,24 @@
 
 set -eo pipefail
 
-export LINE_CONFIG_FILE="ormp_line.c"
-export LINE_DEPLOY_FILE="deploy_ormp_line.a"
-export LINE_KEY=".ORMP_LINE"
+export PORT_CONFIG_FILE="ormp_port.c"
+export PORT_DEPLOY_FILE="deploy_ormp_port.a"
+export PORT_KEY=".ORMP_PORT"
 
 # cid="QmPqyGBHxRCxZJEKf8WTeDZB2JDMrtfrPqxGcxN2scj64E"
 cid="QmZkugpAG9Pfp52yjDAxyA7cVGcDAryMJUXPfFDUbp8iGo"
 cid=$(ipfs cid format -v 1 -b base32 $cid)
 uri="ipfs://$cid"
-# forge script script/config/LineConfig.s.sol:LineConfig --sig "run(uint256[],string)" "[42161]" $uri --chain-id 46    --broadcast --slow
-# forge script script/config/LineConfig.s.sol:LineConfig --sig "run(uint256[],string)" "[46]"    $uri --chain-id 42161 --broadcast --slow --legacy
-# forge script script/config/LineConfig.s.sol:LineConfig --sig "run(uint256[],string)" "[46]"    $uri --chain-id 1 --broadcast --slow --legacy
+# forge script script/config/PortConfig.s.sol:PortConfig --sig "run(uint256[],string)" "[42161]" $uri --chain-id 46    --broadcast --slow
+# forge script script/config/PortConfig.s.sol:PortConfig --sig "run(uint256[],string)" "[46]"    $uri --chain-id 42161 --broadcast --slow --legacy
+# forge script script/config/PortConfig.s.sol:PortConfig --sig "run(uint256[],string)" "[46]"    $uri --chain-id 1 --broadcast --slow --legacy
 
-# forge script script/config/LineConfig.s.sol:LineConfig --sig "run(uint256[],string)" "[421614]"   $uri --chain-id 44       --broadcast
-# forge script script/config/LineConfig.s.sol:LineConfig --sig "run(uint256[],string)" "[11155111]" $uri --chain-id 44       --broadcast
-# forge script script/config/LineConfig.s.sol:LineConfig --sig "run(uint256[],string)" "[11155111]" $uri --chain-id 421614   --broadcast --skip-simulation --legacy
-# forge script script/config/LineConfig.s.sol:LineConfig --sig "run(uint256[],string)" "[44]"       $uri --chain-id 421614   --broadcast --skip-simulation --legacy
-# forge script script/config/LineConfig.s.sol:LineConfig --sig "run(uint256[],string)" "[44]"       $uri --chain-id 11155111 --broadcast
-# forge script script/config/LineConfig.s.sol:LineConfig --sig "run(uint256[],string)" "[421614]"   $uri --chain-id 11155111 --broadcast
+# forge script script/config/PortConfig.s.sol:PortConfig --sig "run(uint256[],string)" "[421614]"   $uri --chain-id 44       --broadcast
+# forge script script/config/PortConfig.s.sol:PortConfig --sig "run(uint256[],string)" "[11155111]" $uri --chain-id 44       --broadcast
+# forge script script/config/PortConfig.s.sol:PortConfig --sig "run(uint256[],string)" "[11155111]" $uri --chain-id 421614   --broadcast --skip-simulation --legacy
+# forge script script/config/PortConfig.s.sol:PortConfig --sig "run(uint256[],string)" "[44]"       $uri --chain-id 421614   --broadcast --skip-simulation --legacy
+# forge script script/config/PortConfig.s.sol:PortConfig --sig "run(uint256[],string)" "[44]"       $uri --chain-id 11155111 --broadcast
+# forge script script/config/PortConfig.s.sol:PortConfig --sig "run(uint256[],string)" "[421614]"   $uri --chain-id 11155111 --broadcast
 
-# forge script script/config/LineConfig.s.sol:LineConfig --sig "run(uint256[],string)" "[11155111]" $uri --chain-id 44       --broadcast
-# forge script script/config/LineConfig.s.sol:LineConfig --sig "run(uint256[],string)" "[44]"       $uri --chain-id 11155111 --broadcast
+# forge script script/config/PortConfig.s.sol:PortConfig --sig "run(uint256[],string)" "[11155111]" $uri --chain-id 44       --broadcast
+# forge script script/config/PortConfig.s.sol:PortConfig --sig "run(uint256[],string)" "[44]"       $uri --chain-id 11155111 --broadcast
--- bin/dao.sh
+++ bin/dao.sh
@@ -6,12 +6,12 @@ c3=$PWD/script/input/c3.json
 
 deployer=$(jq -r ".DEPLOYER" $c3)
 dao=$(jq -r ".MSGDAO" $c3)
-registry=$(jq -r ".LINEREGISTRY_ADDR" $c3)
-ormp_line=$(jq -r ".ORMPLINEEXT_ADDR" $c3)
+registry=$(jq -r ".PORTREGISTRY_ADDR" $c3)
+ormp_port=$(jq -r ".ORMPPORTEXT_ADDR" $c3)
 
 # seth send -F $deployer $registry "transferOwnership(address)" $dao --chain darwinia
 # seth send -F $deployer $registry "transferOwnership(address)" $dao --chain arbitrum
 seth send -F $deployer $registry "transferOwnership(address)" $dao --chain ethereum
 
-# seth send -F $deployer $ormp_line "transferOwnership(address)" $dao --chain darwinia
-# seth send -F $deployer $ormp_line "transferOwnership(address)" $dao --chain arbitrum
+# seth send -F $deployer $ormp_port "transferOwnership(address)" $dao --chain darwinia
+# seth send -F $deployer $ormp_port "transferOwnership(address)" $dao --chain arbitrum
