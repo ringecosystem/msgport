c449dbd23668393b874d041d358d664530dfee0e
echo <hujw77@gmail.com>
2024-02-01 16:58:56 +0800

    support pangolin

--- SUPPORTED.md
+++ SUPPORTED.md
@@ -4,6 +4,7 @@
 | ORMPPort |   ORMP  |   0x1a823de4   | 0x0000000005d961F950adA391C1511c92bbc64D9F |
 
 ## Supported Chains
-- Crab
-- Sepolia
 - Arbitrum Sepolia
+- Crab
+- Pangolin
+- Sepolia
--- bin/deploy.sh
+++ bin/deploy.sh
@@ -14,6 +14,7 @@ set -eo pipefail
 # forge script script/deploy/DeployORMPPortExt.s.sol:DeployORMPPortExt     --chain-id 46          --broadcast --verify
 # forge script script/deploy/DeployORMPPortExt.s.sol:DeployORMPPortExt     --chain-id 42161     --legacy --broadcast --verify --skip-simulation
 
-forge script script/deploy/DeployORMPPort.s.sol:DeployORMPPort         --chain-id 44       --broadcast --verify --slow
-forge script script/deploy/DeployORMPPort.s.sol:DeployORMPPort         --chain-id 421614   --broadcast --verify --slow --legacy --skip-simulation
-forge script script/deploy/DeployORMPPort.s.sol:DeployORMPPort         --chain-id 11155111 --broadcast --verify --slow --legacy
+forge script script/deploy/DeployORMPPort.s.sol:DeployORMPPort         --chain-id 43       --broadcast --verify --slow --legacy --skip-simulation
+# forge script script/deploy/DeployORMPPort.s.sol:DeployORMPPort         --chain-id 44       --broadcast --verify --slow
+# forge script script/deploy/DeployORMPPort.s.sol:DeployORMPPort         --chain-id 421614   --broadcast --verify --slow --legacy --skip-simulation
+# forge script script/deploy/DeployORMPPort.s.sol:DeployORMPPort         --chain-id 11155111 --broadcast --verify --slow --legacy
