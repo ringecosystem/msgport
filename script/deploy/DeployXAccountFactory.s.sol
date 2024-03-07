0fded1a43e9226495f04206f95261b834daec56b
echo <hujw77@gmail.com>
2024-03-07 14:51:21 +0800

    deploy script

--- script/deploy/DeployXAccountFactory.s.sol
+++ script/deploy/DeployXAccountFactory.s.sol
@@ -21,6 +21,8 @@ contract DeployXAccountFactory is Common {
 
     address ADDR;
     bytes32 SALT;
+    address MODULE;
+    address REGISTRY;
 
     string c3;
     string config;
@@ -42,9 +44,37 @@ contract DeployXAccountFactory is Common {
         c3 = ScriptTools.readInput("../c3");
         ADDR = c3.readAddress(".XACCOUNTFACTORY_ADDR");
         SALT = c3.readBytes32(".XACCOUNTFACTORY_SALT");
+        MODULE = c3.readAddress(".SAFEMSGPORTMODULE_ADDR");
+        REGISTRY = c3.readAddress(".PORTREGISTRY_ADDR");
 
         deployer = config.readAddress(".DEPLOYER");
         dao = config.readAddress(".DAO");
+    }
+
+    function readSafeDeployment()
+        internal
+        returns (address proxyFactory, address gnosisSafe, address fallbackHandler)
+    {
+        uint256 chainId = vm.envOr("CHAIN_ID", block.chainid);
+        string memory root = vm.projectRoot();
+        string memory safeFolder = string(abi.encodePacked("/lib/safe-deployments/src/assets/", safeVerison, "/"));
+        string memory proxyFactoryFile = vm.readFile(string(abi.encodePacked(root, safeFolder, "proxy_factory.json")));
+        proxyFactory =
+            proxyFactoryFile.readAddress(string(abi.encodePacked(".networkAddresses.", vm.toString(chainId))));
+        string memory gasisSafeJson;
+        if (chainId.isL2()) {
+            gasisSafeJson = "gnosis_safe_l2.json";
+        } else {
+            gasisSafeJson = "gnosis_safe.json";
+        }
+
+        string memory fallbackHandlerFile =
+            vm.readFile(string(abi.encodePacked(root, safeFolder, "compatibility_fallback_handler.json")));
+        fallbackHandler =
+            fallbackHandleFile.readAddress(string(abi.encodePacked(".networkAddresses.", vm.toString(chainId))));
+
+        string memory gnosisSageFile = vm.readFile(string(abi.encodePacked(root, safeFolder, gasisSafeJson)));
+        gnosisSafe = gnosisSageFile.readAddress(string(abi.encodePacked(".networkAddresses.", vm.toString(chainId))));
     }
 
     function run() public {
@@ -59,8 +89,11 @@ contract DeployXAccountFactory is Common {
 
     function deploy() public broadcast returns (address) {
         string memory name_ = config.readString(".metadata.name");
+        (address safeFactory, address safeSingleton, address safeFallbackHandler) = readSafeDeployment();
         bytes memory byteCode = type(ORMPPort).creationCode;
-        bytes memory initCode = bytes.concat(byteCode, abi.encode(deployer, ORMP, name_));
+        bytes memory initCode = bytes.concat(
+            byteCode, abi.encode(deployer, MODULE, safeFactory, safeSingleton, safeFallbackHandler, REGISTRY, nema_)
+        );
         address port = _deploy3(SALT, initCode);
         require(port == ADDR, "!addr");
         require(III(ADDR).owner() == deployer);
