1a1a23a74a9069b7b84f8ed802b8e396f27da7ff
echo <hujw77@gmail.com>
2024-03-22 21:33:20 +0800

    pin solc version to 0.8.17

--- src/ports/ORMPUpgradeablePort.sol
+++ src/ports/ORMPUpgradeablePort.sol
@@ -15,7 +15,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Darwinia. If not, see <https://www.gnu.org/licenses/>.
 
-pragma solidity ^0.8.17;
+pragma solidity 0.8.17;
 
 import "./base/BaseMessagePort.sol";
 import "./base/PortLookup.sol";
