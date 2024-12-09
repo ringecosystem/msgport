ab9812cf44f2ecb7e4ab468cd9867c7ec43e9196
echo <hujw77@gmail.com>
2024-12-09 16:44:21 +0800

    clean

--- src/interfaces/ISafeMsgportModule.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
-
-interface ISafeMsgportModule {
-    function setup(address xAccount, uint256 chainId, address owner, address port_) external;
-}
